2434d38eb18cd335e42d53b2596591a7abc99003
jijianyong <1992544406@qq.com>
2020-04-21 00:46:16 +0800

    实现选项卡的功能与返回组件

--- javaScript/ui.js
+++ javaScript/ui.js
@@ -18,9 +18,53 @@ $.fn.UiSearch = function() {
     });
 }
 
+/**
+ * 显示选项卡内容
+ * @param {string} header  Tab组件，所有选项卡item
+ * @param {string} content  Tab组件，内容区域，所有item
+ * @param {string} focus  高亮显示
+ */
+$.fn.UiTab = function(header,content,focus) {
+    var ui = $(this);
+    var tabs = $(header);
+    var cons = $(content);
+    var focus = focus || '';
+
+    tabs.on("click", function() {
+        var index = $(this).index();
+        // debugger;
+        tabs.removeClass(focus+'item-focus').eq(index).addClass(focus+'item-focus');
+        cons.hide().eq(index).show();
+        return false;
+    })
+}
+
+// ui-backTop 返回顶部
+$.fn.UiBackTop = function() {
+    var ui = $(this);
+    var windowHeigh = $(window).height()/2;
+    var el = $('<a href="#" class="ui-backTop">返回顶部</a>');
+    ui.append(el);
+
+    $(window).on('scroll', function() {
+        var top = $(window).scrollTop();
+        console.log("A:"+top);
+        console.log("B:"+windowHeigh);
+        if(top > windowHeigh){
+            el.show();
+        }else {
+            el.hide();
+        }
+    })
+}
+
+
 // 页面的脚本逻辑
 $(function() {
     $('.ui-search').UiSearch();
+    $('.content-tab').UiTab('.caption > .item','.block > .item');
+    $('.content-tab .block .item').UiTab('.block-caption > a','.block-content > .block-wrap','block-caption-');
+    $('body').UiBackTop();
 })
 
 
